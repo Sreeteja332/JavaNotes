0f44f9a30dc0107d0091563d06ac8184f1ee7b90
Sreeteja332 <sreeteja332@gmail.com>
2025-02-05 14:44:10 +0530

    updated

---
+++ Ref/D36.java
@@ -0,0 +1,5 @@
+public class D36 {
+    public static void main(String[] args) {
+        System.out.println(args[1]);
+    }
+}
--- Ref/Patient.java
+++ Ref/Patient.java
@@ -19,3 +19,43 @@ public class Patient {
 
     }
 }
+
+// using this keyword.
+
+// import java.util.Scanner;
+
+// public class Patient {
+//     // Instance variables
+//     private String name;
+//     private double weight;
+//     private double height;
+    
+//     // Constructor
+//     public Patient(String name, double weight, double height) {
+//         this.name = name;
+//         this.weight = weight;
+//         this.height = height;
+//     }
+    
+//     public void bmi() {
+//         double bmi = this.weight/(this.height*this.height);
+//         System.out.println("BMI of " + this.name + " is " + bmi);
+//     }
+    
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.print("Enter the Name of the Patient: ");
+//         String name = sc.next();
+        
+//         System.out.print("Enter the Height(in meters) of the Patient: ");
+//         double height = sc.nextDouble();
+        
+//         System.out.print("Enter the Weight(in kgs) of the Patient: ");
+//         double weight = sc.nextDouble();
+        
+//         Patient obj = new Patient(name, weight, height);
+//         obj.bmi();
+//     }
+// }
+//
